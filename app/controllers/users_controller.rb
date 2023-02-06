24ad138a520ebab86d9c9edf2e4a2a1e87e7838f
Jonathan <jonitoadriano@gmail.com>
2023-02-06 13:16:58 -0600

    sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+def create
+  user = User.new(
+    name: params[:name],
+    email: params[:email],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation],
+    image_url: params[:image_url]
+  )
+end
+
 end
