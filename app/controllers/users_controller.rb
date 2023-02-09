5c2f45cf364f7cbdcf5184a7e2cb9841abb28836
Jonathan <jonitoadriano@gmail.com>
2023-02-09 11:33:24 -0600

    user login fixedand profile is dynamic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ end
 
 def show
   # show info if logged in and authorized
-  user = User.find(params[:id])
+  user = User.find(current_user.id)
   name = user.name
   pic = user.image_url
   render json: {name: name.as_json, pic: pic.as_json }
