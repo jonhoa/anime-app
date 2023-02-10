7b4dfa4dbde9e3994a0eb92d992a16f248eeb338
Jonathan <jonitoadriano@gmail.com>
2023-02-10 12:31:14 -0600

    item controller jb show send favorited true or false

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    items = Item.find_by(id: params[:id])
-    render json: items.as_json
+    @item = Item.find_by(id: params[:id])
+    favoritescheck = Favorite.where(user_id: current_user.id ,item_id: @item.id)
+
+    if favoritescheck.length == 0
+      @favorited = false
+      puts "not favorited"
+    else 
+      @favorited = true
+    end
+
+    render template: "items/show"
   end
 end
